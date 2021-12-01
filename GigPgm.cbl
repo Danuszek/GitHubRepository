47021a9aeeb9a4746c4113a2e7178eabf731a0c4
[name] <dante_cola@be.ibm.com>
2021-12-01 10:28:45 +0100

    I delete this pgm

--- GigPgm.cbl
+++
@@ -1,6 +0,0 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID.     GITPGM.
-   
-       PROCEDURE DIVISION.
-           DISPLAY "This is my Git pgm stored in LocalRepositories".
-           STOP RUN.
